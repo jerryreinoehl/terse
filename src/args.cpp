11bf722394d22aa38689f4d8b9bd5820b34977f6
Jerry Reinoehl <jerryreinoehl@gmail.com>
2023-12-30 16:22:17 -0600

    Add `--` argument to pass all remaining arguments as command

--- src/args.cpp
+++ src/args.cpp
@@ -2,16 +2,20 @@
 
 Args::Args(int argc, char **argv) {
   std::string arg;
+  bool end_of_opts = false;
+
   for (char **it = &argv[1], **end = &argv[argc]; it != end; ++it) {
     arg = std::string{*it};
 
-    if (arg[0] != '-') {
+    if (end_of_opts || arg[0] != '-') {
       command_.push_back(arg);
       continue;
     }
 
     if (arg == "-v")
       verbose_ = true;
+    else if (arg == "--")
+      end_of_opts = true;
   }
 }
 
