81f0dcf23badf67a2f124d65cd375d76b0d84134
Jerry Reinoehl <jerryreinoehl@gmail.com>
2024-01-11 22:45:16 -0600

    Remove extraneous namespace

--- src/terse/emission.cpp
+++ src/terse/emission.cpp
@@ -11,11 +11,11 @@ std::vector<std::string> Emission::tokens() const noexcept {
   return tokens_;
 }
 
-void terse::Emission::set_tokens(const std::vector<std::string>& tokens) {
+void Emission::set_tokens(const std::vector<std::string>& tokens) {
   tokens_ = tokens;
 }
 
-std::optional<TokenMap> terse::Emission::map() const noexcept {
+std::optional<TokenMap> Emission::map() const noexcept {
   return map_;
 }
 
