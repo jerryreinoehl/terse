fed8bc6155b48f5a2ef100d9dff38845c0577e4c
Jerry Reinoehl <jerryreinoehl@gmail.com>
2025-08-21 20:41:36 -0500

    Rework token

--- src/terse/token.cpp
+++ src/terse/token.cpp
@@ -4,13 +4,51 @@ using terse::Token;
 using terse::TokenType;
 using terse::WordToken;
 
-Token::Token(TokenType type) : type_{type} {}
+TokenType::Value TokenType::next_value{0};
+std::map<std::string_view, TokenType> TokenType::map{};
+std::vector<std::string_view> TokenType::names{};
 
-TokenType Token::type() const noexcept {
+const TokenType TokenType::NONE{"NONE"};
+const TokenType TokenType::ARM{"=>", "ARM"};
+const TokenType TokenType::BRACE_LEFT{"{", "BRACE_LEFT"};
+const TokenType TokenType::BRACE_RIGHT{"}", "BRACE_RIGHT"};
+const TokenType TokenType::DOUBLE_QUOTE{"\"", "DOUBLE_QUOTE"};
+const TokenType TokenType::FUNC{"$", "FUNC"};
+const TokenType TokenType::NEWLINE{"\n", "NEWLINE"};
+const TokenType TokenType::PAREN_LEFT{"(", "PAREN_LEFT"};
+const TokenType TokenType::PAREN_RIGHT{")", "PAREN_RIGHT"};
+const TokenType TokenType::STOP{"STOP"};
+const TokenType TokenType::WORD{"WORD"};
+
+TokenType TokenType::from_lexeme(const std::string& lexeme) {
+  auto it = map.find(lexeme);
+
+  if (it != map.end()) {
+    return it->second;
+  }
+
+  return NONE;
+}
+
+std::ostream& terse::operator<<(std::ostream& out, TokenType type) {
+  return out << type.to_string();
+}
+
+Token::Token(TokenType type, int line, int col) noexcept : type_{type}, line_{line}, col_{col} {}
+
+const TokenType Token::type() const noexcept {
   return type_;
 }
 
-WordToken::WordToken(const std::string& value) : Token{TokenType::WORD}, value_{value} {}
+int Token::line() const noexcept {
+  return line_;
+}
+
+int Token::col() const noexcept {
+  return col_;
+}
+
+WordToken::WordToken(const std::string& value, int line, int col) : Token{TokenType::WORD, line, col}, value_{value} {}
 
 std::string WordToken::value() const noexcept {
   return value_;
--- src/terse/token.h
+++ src/terse/token.h
@@ -1,35 +1,80 @@
 #pragma once
 
+#include <iostream>
+#include <map>
 #include <string>
+#include <string_view>
+#include <vector>
 
 namespace terse {
 
-  enum class TokenType {
-    ARM,
-    BRACE_LEFT,
-    BRACE_RIGHT,
-    DOUBLE_QUOTE,
-    FUNC,
-    NEWLINE,
-    PAREN_LEFT,
-    PAREN_RIGHT,
-    STOP,
-    WORD,
+  class TokenType {
+    public:
+      static const TokenType NONE;
+      static const TokenType ARM;
+      static const TokenType BRACE_LEFT;
+      static const TokenType BRACE_RIGHT;
+      static const TokenType DOUBLE_QUOTE;
+      static const TokenType FUNC;
+      static const TokenType NEWLINE;
+      static const TokenType PAREN_LEFT;
+      static const TokenType PAREN_RIGHT;
+      static const TokenType STOP;
+      static const TokenType WORD;
+
+      static TokenType from_lexeme(const std::string& lexeme);
+
+      TokenType() noexcept {}
+
+      TokenType(const std::string_view name) : value_{next_value++} {
+        names.push_back(name);
+      }
+
+      TokenType(const std::string_view lexeme, const std::string_view name) : TokenType{name} {
+        map[lexeme] = *this;
+      }
+
+      std::string to_string() const noexcept {
+        return std::string{names[value_]};
+      }
+
+      bool operator==(const TokenType other) const noexcept {
+        return value_ == other.value_;
+      }
+
+      bool operator!=(const TokenType other) const noexcept {
+        return value_ != other.value_;
+      }
+
+    private:
+      using Value = unsigned char;
+
+      static Value next_value;
+      static std::map<std::string_view, TokenType> map;
+      static std::vector<std::string_view> names;
+
+      Value value_;
   };
+
+  std::ostream& operator<<(std::ostream& out, TokenType type);
 
   class Token {
     public:
-      Token(TokenType type);
+      Token(TokenType type, int line, int col) noexcept;
 
-      TokenType type() const noexcept;
+      const TokenType type() const noexcept;
+      int line() const noexcept;
+      int col() const noexcept;
 
     private:
       TokenType type_;
+      int line_;
+      int col_;
   };
 
   class WordToken : public Token {
     public:
-      WordToken(const std::string& value);
+      WordToken(const std::string& value, int line, int col);
 
       std::string value() const noexcept;
 
